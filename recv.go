0f585d1b93400481b856af800b83d5667240ff5d
pyra <pyraninja@gmail.com>
2015-01-31 01:09:31 +0200

    added code

---
+++ recv.go
@@ -0,0 +1,274 @@
+package udpsender
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+type RecvService struct {
+	key          string
+	ch           chan *RecvBlock
+	transactions map[string]*RecvTransaction
+	sock         *net.UDPConn
+}
+
+type RecvTransaction struct {
+	num         int
+	blocks      map[int]*RecvBlock
+	lastTime    int64
+	blockSize   int64
+	speed       float64
+	completed   bool
+	addr        *net.UDPAddr
+	transaction []byte
+}
+
+type RecvBlock struct {
+	transaction      []byte
+	blockHash        []byte
+	fileHash         []byte
+	fileData         []byte
+	num, id          int32
+	start, end, size int64
+	time1            int64
+	addr             *net.UDPAddr
+}
+
+func InitRecvService(key string, url string) (service *RecvService, err error) {
+	service = new(RecvService)
+	service.key = key
+	service.ch = make(chan *RecvBlock, 100)
+	service.transactions = map[string]*RecvTransaction{}
+
+	addr, err := net.ResolveUDPAddr("udp", url)
+	if err != nil {
+		return nil, err
+	}
+	service.sock, err = net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+	go service.loop()
+
+	return service, nil
+}
+
+func InitRecvTransaction(block *RecvBlock) *RecvTransaction {
+	transaction := new(RecvTransaction)
+	transaction.blocks = map[int]*RecvBlock{}
+	transaction.lastTime = time.Now().UnixNano()
+	transaction.blockSize = int64(len(block.fileData))
+	transaction.num = int(block.num)
+	transaction.addr = block.addr
+	transaction.transaction = block.transaction
+	return transaction
+}
+func (service *RecvService) processRecvBlock(block *RecvBlock) {
+	stransaction := hex.EncodeToString(block.transaction)
+	transaction, ok := service.transactions[stransaction]
+	if !ok {
+		fmt.Println("Creating transaction:", stransaction)
+		transaction = InitRecvTransaction(block)
+		service.transactions[stransaction] = transaction
+	}
+	if transaction.num != int(block.num) {
+		// error
+	}
+	block.time1 = time.Now().UnixNano()
+	fdtime := float64(block.time1 - transaction.lastTime)
+	transaction.lastTime = block.time1
+	speed := float64(len(block.fileData)) / fdtime * float64(time.Second)
+	transaction.speed = movingExpAvg(speed, transaction.speed, fdtime, float64(time.Second))
+
+	_, ok = transaction.blocks[int(block.id)]
+	if ok {
+		// dupe
+		return
+	}
+	transaction.blocks[int(block.id)] = block
+	// fmt.Printf("got %d %d %d\n%s\n", block.id, len(transaction.blocks), transaction.num, string(block.fileData))
+	fmt.Printf("got %d %d %d %f\n", block.id, len(transaction.blocks), transaction.num, transaction.speed)
+	if len(transaction.blocks) == transaction.num {
+		transaction.completed = true
+		fmt.Printf("Finish RecvTransaction %f\n", transaction.speed)
+		// finish transaction
+		// save file
+
+		f1, err := os.Create("incomming/" + stransaction + "/file.out")
+		if err != nil {
+			panic(err)
+		}
+		// defer f1.Close()
+
+		for i := 1; i <= transaction.num; i++ {
+			block1, ok := transaction.blocks[i]
+			if !ok {
+				// error
+				panic(errors.New(fmt.Sprintf("Not all blocks %d", i)))
+			}
+			f1.Write(block1.fileData)
+		}
+		f1.Close()
+		// file recieved
+	}
+}
+func (service *RecvService) Loop() {
+
+	for {
+		// len, remote, err := sock.ReadFromUDP(buf[:])
+		var buf [1 << 16]byte
+		len, remote, err := service.sock.ReadFromUDP(buf[:])
+		if err != nil {
+			panic(err)
+		}
+		service.processPacket(remote, buf[:len])
+	}
+}
+func (service *RecvService) loop() {
+	ticker := time.Tick(100 * time.Millisecond)
+	for {
+
+		select {
+		case blck, ok := <-service.ch:
+			if ok {
+				service.processRecvBlock(blck)
+			}
+		case <-ticker:
+			time1 := time.Now().UnixNano()
+			for _, transaction := range service.transactions {
+				if transaction.completed {
+					continue
+				}
+
+				fdtime := float64(time1 - transaction.lastTime)
+				transaction.lastTime = time1
+				transaction.speed = movingExpAvg(0.0, transaction.speed, fdtime, float64(time.Second))
+
+				//
+				// find last_block
+				last_block := -1
+				var block *RecvBlock
+				for i, _block := range transaction.blocks {
+					if last_block < i {
+						last_block = i
+						block = _block
+					}
+				}
+				if last_block == -1 {
+					continue
+				}
+				// dtime  = Now - last_block.time
+				dtime := time1 - block.time1
+				// last_block + (dtime-100ms)*speed/blocksize
+				shift := last_block + int((float64(dtime)-100.0*float64(time.Millisecond))*transaction.speed/float64(transaction.blockSize))
+				// get first 100 un recieved blocks
+
+				req := []int{}
+
+				for i := 1; i <= transaction.num; i++ {
+					if i > shift {
+						break
+					}
+					_, ok := transaction.blocks[i]
+					if !ok {
+						req = append(req, i)
+						if len(req) >= 100 {
+							break
+						}
+					}
+				}
+
+				if len(req) > 0 {
+					service.rerequestPackets(transaction, req)
+				}
+			}
+		}
+
+	}
+
+}
+func (service *RecvService) rerequestPackets(transaction *RecvTransaction, req []int) {
+	r1 := make([]byte, 16)
+	_, err := rand.Read(r1)
+	if err != nil {
+		panic(err)
+	}
+
+	data := append(transaction.transaction, r1...)
+
+	buffer := new(bytes.Buffer)
+	binary.Write(buffer, binary.LittleEndian, int32(1))
+	binary.Write(buffer, binary.LittleEndian, int32(len(req)))
+	for _, i := range req {
+		binary.Write(buffer, binary.LittleEndian, int32(i))
+	}
+
+	block, err := aes.NewCipher([]byte(service.key))
+	if err != nil {
+		panic(err)
+	}
+
+	data1 := buffer.Bytes()
+
+	stream := cipher.NewCFBEncrypter(block, r1)
+	stream.XORKeyStream(data1, data1)
+
+	data = append(data, data1...)
+
+	service.sock.WriteToUDP(data, transaction.addr)
+}
+func (service *RecvService) processPacket(addr *net.UDPAddr, buf []byte) {
+	blck := new(RecvBlock)
+	blck.transaction = buf[:16]
+	blck.addr = addr
+	r1 := buf[16:32]
+	data2 := buf[32:]
+
+	block, err := aes.NewCipher([]byte(service.key))
+	if err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBDecrypter(block, r1)
+
+	stream.XORKeyStream(data2, data2)
+
+	// fileHash := data2[0:20]
+
+	buffer := bytes.NewBuffer(data2[20:52])
+	binary.Read(buffer, binary.LittleEndian, &blck.id)
+	binary.Read(buffer, binary.LittleEndian, &blck.num)
+	binary.Read(buffer, binary.LittleEndian, &blck.start)
+	binary.Read(buffer, binary.LittleEndian, &blck.end)
+	binary.Read(buffer, binary.LittleEndian, &blck.size)
+	blck.blockHash = data2[52:72]
+	blck.fileData = data2[72:]
+	blockHash := sha1.Sum(blck.fileData)
+	verified := bytes.Equal(blck.blockHash, blockHash[:])
+
+	fmt.Println("processPacker:", blck.id, hex.EncodeToString(r1))
+
+	if verified {
+		os.MkdirAll("incomming/"+hex.EncodeToString(blck.transaction), 0700)
+		f1, err := os.Create("incomming/" + hex.EncodeToString(blck.transaction) + "/" + fmt.Sprintf("%d.data", blck.id))
+		if err != nil {
+			panic(err)
+		}
+		defer f1.Close()
+		f1.Write(blck.fileData)
+		service.ch <- blck
+	} else {
+		fmt.Println("wrong hash %#v %#v", blck.blockHash, blockHash[:])
+	}
+
+}
---
+++ sender.go
@@ -0,0 +1,253 @@
+package udpsender
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"crypto/sha1"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+type SendService struct {
+	conn        *net.UDPConn
+	addr        *net.UDPAddr
+	filename    string
+	blockSize   int64
+	size        int64
+	num         int64
+	reminder    int64
+	key         string
+	transaction []byte
+	fileHash    []byte
+	f1          *os.File
+	speed       int64
+}
+
+func InitSendService(speed int64, from string, to string) (service *SendService, err error) {
+	service = new(SendService)
+	service.speed = speed
+
+	addr, err := net.ResolveUDPAddr("udp", from)
+	if err != nil {
+		return nil, err
+	}
+	service.addr, err = net.ResolveUDPAddr("udp", to)
+	if err != nil {
+		return nil, err
+	}
+	service.conn, err = net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	// conn, err := net.Dial("udp", "127.0.0.1:12345")
+
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// service.conn = conn
+
+	return service, nil
+}
+func (service *SendService) SendFile(key string, filename string) {
+	fmt.Println("SendFile")
+	service.filename = filename
+	service.key = key
+
+	service.transaction = make([]byte, 16)
+	_, err := rand.Read(service.transaction)
+	if err != nil {
+		panic(err)
+	}
+
+	service.blockSize = 29999
+	// service.blockSize = 10
+
+	fi, err := os.Stat(filename)
+	if err != nil {
+		panic(err)
+	}
+	service.size = fi.Size()
+	service.num = service.size / service.blockSize
+	service.reminder = service.size % service.blockSize
+
+	service.f1, err = os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+	//defer f1.Close()
+
+	h := sha1.New()
+
+	buf := make([]byte, 1<<16)
+	for {
+		n, err := service.f1.Read(buf)
+
+		if err != nil {
+			// panic(err)
+			// EOF usually
+			break
+		}
+		if n == 0 {
+			break
+		}
+		h.Write(buf[:n])
+	}
+
+	service.fileHash = h.Sum(nil)
+
+	fmt.Println("Transaction: " + hex.EncodeToString(service.transaction))
+	fmt.Println("File hash: " + hex.EncodeToString(service.fileHash))
+	fmt.Printf("num: %d reminder: %d\n", service.num, service.reminder)
+	// send info block
+	// send blocks
+	num := service.num
+	if service.reminder > 0 {
+		service.num++
+	}
+	time1 := time.Now().UnixNano()
+
+	for i := int64(1); i <= num; i++ {
+		service.sendBlock1(int(i)) //, service.blockSize*(i-1), service.blockSize*i)
+		time2 := time.Now().UnixNano()
+		dt1 := time2 - time1
+		time1 = time2
+		dt2 := int64(time.Second) * service.blockSize / service.speed
+		dt3 := dt2 - dt1
+		if dt3 > 0 {
+			time.Sleep(time.Duration(dt3))
+		}
+
+		// time.Sleep(10 * time.Millisecond)
+	}
+	// send reminder
+	if service.reminder > 0 {
+		service.sendBlock1(int(num + 1)) //, service.blockSize*num, service.blockSize*num+reminder)
+	}
+
+	for {
+		// len, remote, err := sock.ReadFromUDP(buf[:])
+		var buf [1 << 16]byte
+		n, _, err := service.conn.ReadFromUDP(buf[:])
+		if err != nil {
+			panic(err)
+		}
+
+		//go service.loop()
+		service.processPacket(buf[:n])
+
+	}
+
+}
+func (service *SendService) processPacket(buf []byte) {
+	// transaction := buf[:16]
+	r1 := buf[16:32]
+
+	data2 := buf[32:]
+
+	block, err := aes.NewCipher([]byte(service.key))
+	if err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBDecrypter(block, r1)
+
+	stream.XORKeyStream(data2, data2)
+
+	buffer := bytes.NewBuffer(data2)
+
+	var type1 int32
+
+	for {
+		err = binary.Read(buffer, binary.LittleEndian, &type1)
+		if err != nil {
+			break
+		}
+		if type1 == 1 {
+			var num int32
+			err = binary.Read(buffer, binary.LittleEndian, &num)
+			if err != nil {
+				break
+			}
+			for {
+				var id int32
+				err = binary.Read(buffer, binary.LittleEndian, &id)
+				if err != nil {
+					break
+				}
+				fmt.Printf("Extra Resend %d\n", id)
+				service.sendBlock1(int(id))
+			}
+
+		}
+	}
+
+}
+func (service *SendService) sendBlock1(id int) {
+	if service.reminder == 0 {
+		service.sendBlock(id, service.blockSize*int64(id-1), service.blockSize*int64(id))
+	} else {
+		if int64(id) == service.num {
+			service.sendBlock(id, service.blockSize*int64(id-1), service.blockSize*int64(id-1)+service.reminder)
+		} else {
+			service.sendBlock(id, service.blockSize*int64(id-1), service.blockSize*int64(id))
+		}
+	}
+
+}
+func (service *SendService) sendBlock(id int, start int64, end int64) {
+	r1 := make([]byte, 16)
+	_, err := rand.Read(r1)
+	if err != nil {
+		panic(err)
+	}
+
+	service.f1.Seek(start, 0)
+	fileData := make([]byte, end-start)
+	n, err := service.f1.Read(fileData)
+	if err != nil {
+		panic(err)
+	}
+	if n != len(fileData) {
+		panic(errors.New(fmt.Sprintf("failed to read all bytes %d %d %d", id, n, len(fileData))))
+	}
+	blockHash := sha1.Sum(fileData)
+
+	data := append(service.transaction, r1...)
+
+	buffer := new(bytes.Buffer)
+
+	buffer.Write(service.fileHash)
+	binary.Write(buffer, binary.LittleEndian, int32(id))
+	binary.Write(buffer, binary.LittleEndian, int32(service.num))
+	binary.Write(buffer, binary.LittleEndian, start)
+	binary.Write(buffer, binary.LittleEndian, end)
+	binary.Write(buffer, binary.LittleEndian, service.size)
+	buffer.Write(blockHash[:])
+	buffer.Write(fileData)
+
+	block, err := aes.NewCipher([]byte(service.key))
+	if err != nil {
+		panic(err)
+	}
+
+	data1 := buffer.Bytes()
+
+	stream := cipher.NewCFBEncrypter(block, r1)
+	stream.XORKeyStream(data1, data1)
+
+	data = append(data, data1...)
+
+	service.conn.WriteToUDP(data, service.addr)
+
+	fmt.Println("sent block ", id, hex.EncodeToString(r1))
+
+}
